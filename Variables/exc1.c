2f3a616b0ed30e03eb79dd629e0f3e11eeeb3986
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-29 22:50:58 +0300

    float exercise

---
+++ Variables/exc1.c
@@ -0,0 +1,13 @@
+// create a float variable (number) with value 78.32
+// print the (number)
+
+#include <stdio.h>
+
+int main(){
+    float number = 78.32f;
+
+    printf("%f", number);
+
+    return 0;
+}
+
