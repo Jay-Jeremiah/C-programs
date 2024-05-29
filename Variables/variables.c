dab36042c9fa010f3f7c9eb51e043111a7a48d7a
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-29 22:29:11 +0300

    variables in c

---
+++ Variables/variables.c
@@ -0,0 +1,14 @@
+// variables are containers to store data like numbers and characters
+
+#include <stdio.h>
+
+int main(){
+
+    //create a variable
+    int age = 25;
+
+    //print the variable
+    printf("%d",age);
+
+    return 0;
+}
