bf08c6b199e08ffdbcd6970938134ff36ddd4e27
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-30 09:23:04 +0300

    printing on the next line

---
+++ Variables/2vars.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float num1 = 45.8;
+    int num2 = 67;
+
+    //print the first variable
+    printf("%f", num1);
+
+    //print a new line
+    printf("\n");
+
+    //print the 2nd variable
+    printf("%d", num2);
+
+    return 0;
+}
