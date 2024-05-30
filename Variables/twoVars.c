8d02277349d984f8916c729db89675ed04bd4a17
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-05-30 09:15:06 +0300

    using two Variables

---
+++ Variables/twoVars.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float num1 = 45.78;
+    int num2 = 78;
+
+    //displaying two number variables, we should note that there is a space between the 
+    //first placeholder and its commas
+    printf("%f ",num1);
+    printf("%d",num2);
+
+    return 0;
+}
