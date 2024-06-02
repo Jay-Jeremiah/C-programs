bc9ca65241c2ad8f17d9535e42fd25f0312b45b5
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 07:42:42 +0300

    being specific on decimals on float

---
+++ data types/decimals.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float decimal = 56.789;
+
+    printf("%f\n",decimal);  //prints 6 digits after decimal point.
+
+    printf("%.1f",decimal);  //prints 1 digit after decimal point
+
+    printf("%.2f",decimal);  //prints 2 digits after decimal point
+
+    return 0;
+}
