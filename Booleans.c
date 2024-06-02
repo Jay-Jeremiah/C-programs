6001001a2b2c90647ef7d60aec8624a39143ecd5
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 10:43:55 +0300

    booleans in C

---
+++ Booleans.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//also include the boolean library
+#include <stdbool.h>
+
+int main(){
+    bool isCodingGood = true;
+    bool isCodingBad = false;
+
+    printf("%d\n",isCodingGood);  //returns 1 when statement is true
+
+    printf("%d\n",isCodingBad);   //returns 0 when statement is false
+
+    return 0;
+
+}
