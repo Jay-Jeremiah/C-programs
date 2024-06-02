5a825bea4f6acb8f8338cbbb040fc2629811ba3e
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 07:05:13 +0300

    character data tyoe in C

---
+++ data types/character.c
@@ -0,0 +1,20 @@
+//character data type is used to store a single character
+
+#include <stdio.h>
+
+int main(){
+
+    char myGrade = 'A';
+
+    printf("I got an %c",myGrade);
+
+    printf("\n");
+
+    //to print multiple characters, use strings
+
+    char myColour[] = "Green";
+    printf("My favourite colour is %s",myColour);
+
+    return 0;
+
+}
