2c77c52d16694840d10b170574e155225c1fbb57
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-02 09:40:51 +0300

    constants In C

---
+++ onstants.c
@@ -0,0 +1,16 @@
+/*Constants are variables that when declared they cannot be changed*/
+
+#include <stdio.h>
+
+int main(){
+
+    const int WorkHours = 24;
+
+    const  float pi = 3.14;
+
+    printf("A day has %d\n",WorkHours);
+
+    printf("The value for pi is %.2f",pi);
+
+    return 0;
+}
