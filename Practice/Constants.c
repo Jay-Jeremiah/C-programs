0e6e10ac4268c75f3ed54204bf87db3a6e9cbcc0
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-26 20:39:50 +0300

    Practice makes perfect

---
+++ Practice/Constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	const int a = 23;
+	const float p = 1.67;
+	
+	float div = a/p;
+	printf("The result is %f.2f",div);
+	return 0;
+}
---
+++ Practice/int.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 78;
+    int num2 = 45;
+
+    int add = num1 + num2;
+    printf("The sum is %d",add);
+    printf("\n");
+
+    int sub = num1 - num2;
+    printf("The difference is %d",sub,"\n");
+    printf("\n");
+
+    int prod = num1*num2;
+    printf("The product is %d",prod,"\n");
+    printf("\n");
+
+    int div = num1/num2;
+    printf("The division is %d",div,"\n");
+    printf("\n");
+
+
+    return 0;
+}
