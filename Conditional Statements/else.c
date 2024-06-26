102f674a059d93d73e21f2b84703112afc4c0ee8
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-26 21:02:59 +0300

    else statement

---
+++ Conditional Statements/else.c
@@ -0,0 +1,17 @@
+// else - specifies a block of code to be executed to be false
+// used with the if statement
+
+#include <stdio.h>
+
+int main(){
+    char Name[] = "Harry";
+
+    if(Name == "Moses"){
+        printf("You're a student at UCU");
+    } else {
+        printf("You are not a student of UCU");
+    }
+
+    return 0;
+}
+
