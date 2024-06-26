9806e0363cf1ba7aa45e035a62462169bd7aa8da
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-26 20:51:20 +0300

    if statement

---
+++ Conditional Statements/if.c
@@ -0,0 +1,14 @@
+// if - specifies whether a block of code to be executed is true
+
+#include <stdio.h>
+
+int main(){
+    float a = 1.66;
+    int b = 1;
+    
+    if(a>b){
+        printf("a is greater than b");
+    }
+
+    return 0;
+}
