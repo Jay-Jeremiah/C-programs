ce3325b35546974f0e8e3234212f5f65b24441a3
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-27 19:19:37 +0300

    if and else statements

---
+++ Practice/Conditionals/if.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	double pi = 3.14;
+	if(pi == 3.14){
+		printf("The answer is right");
+		printf("\n");
+	} else {
+		printf("The answer is wrong");
+	}
+	return 0;
+}
