50594e3a878cd6907c2276a3ee45ce08d7f6b7ba
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-27 19:43:33 +0300

    if and else Practice

---
+++ Practice/Conditionals/if3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int myAge = 26;
+    int VoteAge = 18;
+
+    if(myAge >= VoteAge){
+        printf("You are allowed to vote");
+    } else {
+        printf("Not old enough to vote");
+    }
+}
