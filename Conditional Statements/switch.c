c622d2654d3252acb8b8ffd8b09aee43edb1e0d7
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-06-28 23:39:34 +0300

    switch

---
+++ Conditional Statements/switch.c
@@ -0,0 +1,25 @@
+//switch - selects one of the code to be executed
+
+#include <stdio.h>
+
+int main(){
+    int day = 4;
+    switch (day){
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thursday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+    }
+    return 0;
+}
---
+++ Operators/asignment.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int x = 9;
+    int y =7;
+
+    int total = x+y;
+    printf("The total is %d",total);
+
+    
+    return 0;
+}
