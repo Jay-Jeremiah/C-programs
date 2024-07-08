2d91f4187acf49c575958b317b245156d472bc18
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-08 09:13:56 +0300

    Arrays in C

--- Arrays/NumberList.c
+++ Arrays/NumberList.c
@@ -1,10 +1,17 @@
 // arrays - used to store multiple values in a single variable.
-//Like any number variable, we should specify the data type.
+/*Like any number variable, we should specify the data type and variable name,
+followed by square brackets*/
 
 #include <stdio.h>
 
 int main(){
     int numbers[] = {25,50, 75, 100};
-    
+    printf("%d",numbers[0]);
+
+    //change array element
+    printf("%d",numbers[1]);      //prints the second number of the list
+
+    numbers[1] = 67;
+    printf("%d",numbers[1]);
     return 0;
 }
