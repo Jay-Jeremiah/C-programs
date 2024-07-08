7aa0112190872a74a465be2af3cc527246f1d4a9
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-08 09:06:20 +0300

    Arrays in C

---
+++ Arrays/NumberList.c
@@ -0,0 +1,10 @@
+// arrays - used to store multiple values in a single variable.
+//Like any number variable, we should specify the data type.
+
+#include <stdio.h>
+
+int main(){
+    int numbers[] = {25,50, 75, 100};
+    
+    return 0;
+}
