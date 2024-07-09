10ddac4548fe38fc742361b4fad97ecd96617c36
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-09 10:25:40 +0300

    Add to list

---
+++ Arrays/Add2List.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int numericals[] = {};       //initial value
+
+ 
+
+    numericals[0] = 83;
+    numericals[1] = 76;
+    numericals[2] = 672;
+
+    printf("%d\n",numericals[2]);
+    return 0;
+}
