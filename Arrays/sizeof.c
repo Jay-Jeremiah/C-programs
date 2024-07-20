bdee238d1b94b85f089baf83891c52fca65b70d8
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-20 08:25:47 +0300

    Array Size

--- Arrays/sizeof.c
+++ Arrays/sizeof.c
@@ -5,6 +5,16 @@
 int main(){
     int myNumbers[] = {20, 10, 50, 56};
     printf("%lu", sizeof(myNumbers));
+    printf("\n");
+
+    printf("%lu",sizeof(myNumbers[0]));
+    printf("\n");
+
+    int length = sizeof(myNumbers) /sizeof(myNumbers[0]);
+
+    printf("%d",length);
+    
+
 
     return 0;
 
