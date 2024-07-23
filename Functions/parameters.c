46c52f972f08f76f23fcb576addfd92d22efccb0
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-24 00:57:36 +0300

    parameters in functions

--- Functions/parameters.c
+++ Functions/parameters.c
@@ -2,13 +2,14 @@
 
 // written in the parenthesis of the function name separated by a comma 
 
-void myName(char name[]){                    //name is the parameter
+void myName(char name[], int age){                    //name is the parameter
     printf("Hello %s\n",name);
+    printf("How old are you %s?\n",name);
+    scanf("%d",&age);
+    printf("You are %d years old.\n Thank you",age);
 }
 
 int main(){
-    myName("Harry");
-    myName("Jay");
-    myName("Jeremiah");
+    myName("Harry", 15);
     return 0;
 }
