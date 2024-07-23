b7aec4849dbcfbc71a319755b6d1dd445bfaa36c
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-23 23:22:56 +0300

    function that calculates sum in C

---
+++ Practice/Functions/calculateSum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void addition(){
+    int x = 5;
+    int y =7;
+    int sum = x + y;
+    printf("The sum is: %d\n",sum);
+}
+
+int main(){
+    addition();
+    return 0;
+}
