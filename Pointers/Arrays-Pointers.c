1476433155610e64b1cea2e18e65453026046428
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-23 22:15:16 +0300

    Pointers in arrays

--- Pointers/Arrays-Pointers.c
+++ Pointers/Arrays-Pointers.c
@@ -2,6 +2,14 @@
 
 int main(){
     // pointers can also be used in arrays
+
+    int myNums[3] = {24,67,79};
+
+    printf("%p\n", myNums);        //memory address of the list is the same as the address of the first element in the list.
+
+    printf("%p\n",&myNums[0]);
+
+    printf("%p\n",&myNums[1]);
     return 0;
 
 }
