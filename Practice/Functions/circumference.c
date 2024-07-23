61595b14454b4330bfef0d6231225973bab2425c
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-24 00:34:33 +0300

    circumference of a circle

--- Practice/Functions/circumference.c
+++ Practice/Functions/circumference.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void area(float pi, int rad){
+void circumference(float pi, int rad){
     double area = 2*pi*rad;
     printf("The area is %.2f",area);
 }
 
 int main(){
-    area(3.14,6);
+    circumference(3.14,6);
     return 0;
 }
