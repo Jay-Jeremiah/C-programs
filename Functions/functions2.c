7577f1d33c441bfe8e2c966b8bd91e84a6a6a1ce
Jay-Jeremiah <jayjeremiahharry@gmail.com>
2024-07-23 23:18:20 +0300

    functions in c

---
+++ Functions/functions2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void myName(){
+    printf("Harry Jay\n");
+    otherName();
+}
+
+void otherName(){
+    printf("Jeremiah\n");
+}
+
+int main(){
+    myName();
+    return 0;
+
+}
